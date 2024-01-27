2263dc044a21e61b29bfc5cda5257016db9a6f3e
José Fernández Ramos <jframos@fastmail.com>
2024-01-27 09:13:40 +0100

    Replace explicit tests with doctest with example usage

--- lib/todo.ex
+++ lib/todo.ex
@@ -1,4 +1,19 @@
 defmodule TodoList do
+  @moduledoc """
+  Todo list implementation.
+
+  ## Example
+
+  iex> todo_list = TodoList.new() |>
+  ...>   TodoList.add_entry(~D[2024-01-27], "Dentist") |>
+  ...>   TodoList.add_entry(~D[2024-01-28], "Shopping") |>
+  ...>   TodoList.add_entry(~D[2024-01-27], "Movies")
+  iex> TodoList.entries(todo_list, ~D[2024-01-27])
+  ["Movies", "Dentist"]
+  iex> TodoList.entries(todo_list, ~D[2024-01-29])
+  []
+
+  """
   def new(), do: %{}
 
   def add_entry(todo_list, date, title) do
--- test/todo_test.exs
+++ test/todo_test.exs
@@ -1,33 +1,4 @@
 defmodule TodoTest do
   use ExUnit.Case
-
-  test "creating a new Todo list initializes an empty map" do
-    assert TodoList.new() == %{}
-  end
-
-  test "adding an entry to an empty Todo list" do
-    date = ~D[2024-01-27]
-    todo_list = TodoList.new()
-    assert TodoList.add_entry(todo_list, date, "Hello world!") == %{date => ["Hello world!"]}
-  end
-
-  test "prepending an entry to existing date entries" do
-    date = ~D[2024-01-27]
-    todo_list = TodoList.new()
-    todo_list = TodoList.add_entry(todo_list, date, "Hello world!")
-
-    assert TodoList.add_entry(todo_list, date, "Another task") == %{
-             date => ["Another task", "Hello world!"]
-           }
-  end
-
-  test "retrieving titles for an existing date" do
-    date = ~D[2024-01-27]
-    todo_list = TodoList.add_entry(%{}, date, "Hello world!")
-    assert TodoList.entries(todo_list, date) == ["Hello world!"]
-  end
-
-  test "retrieving titles for non-existing date returns an empty list" do
-    assert TodoList.entries(%{}, ~D[2024-01-27]) == []
-  end
+  doctest TodoList
 end
