d3d204128380c1da3bc58623f3bb28717f0a69a1
José Fernández Ramos <jframos@fastmail.com>
2024-01-29 16:44:15 +0100

    Represent `TodoList` entries with a map

--- lib/todo.ex
+++ lib/todo.ex
@@ -5,19 +5,19 @@ defmodule TodoList do
   ## Example
 
   iex> todo_list = TodoList.new() |>
-  ...>   TodoList.add_entry(~D[2024-01-27], "Dentist") |>
-  ...>   TodoList.add_entry(~D[2024-01-28], "Shopping") |>
-  ...>   TodoList.add_entry(~D[2024-01-27], "Movies")
+  ...>   TodoList.add_entry(%{date: ~D[2024-01-27], title: "Dentist"}) |>
+  ...>   TodoList.add_entry(%{date: ~D[2024-01-28], title: "Shopping"}) |>
+  ...>   TodoList.add_entry(%{date: ~D[2024-01-27], title: "Movies"})
   iex> TodoList.entries(todo_list, ~D[2024-01-27])
-  ["Movies", "Dentist"]
+  [%{date: ~D[2024-01-27], title: "Movies"}, %{date: ~D[2024-01-27], title: "Dentist"}]
   iex> TodoList.entries(todo_list, ~D[2024-01-29])
   []
 
   """
   def new(), do: MultiDict.new()
 
-  def add_entry(todo_list, date, title) do
-    MultiDict.add(todo_list, date, title)
+  def add_entry(todo_list, entry) do
+    MultiDict.add(todo_list, entry.date, entry)
   end
 
   def entries(todo_list, date) do
