60cb34ca92a197fc66d251b0b986bde6c69e8839
José Fernández Ramos <jframos@fastmail.com>
2024-02-01 14:08:55 +0100

    Forbid updating an entry ID with `update_entry/3`

--- lib/todo.ex
+++ lib/todo.ex
@@ -50,7 +50,8 @@ defmodule TodoList do
         todo_list
 
       {:ok, old_entry} ->
-        new_entry = update_fun.(old_entry)
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = update_fun.(old_entry)
         new_entries = Map.put(todo_list.entries, entry_id, new_entry)
         %TodoList{todo_list | entries: new_entries}
     end
--- test/todo_test.exs
+++ test/todo_test.exs
@@ -1,4 +1,12 @@
 defmodule TodoTest do
   use ExUnit.Case
   doctest TodoList
+
+  test "updating an entry should not allow modification of its ID" do
+    todo_list =
+      TodoList.new()
+      |> TodoList.add_entry(%{date: ~D[2024-01-27], title: "Dentist"})
+
+    catch_error(TodoList.update_entry(todo_list, 1, &Map.put(&1, :id, 5)))
+  end
 end
