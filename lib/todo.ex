1bda5215d3b265accb54115e8c5b71b1be975899
José Fernández Ramos <jframos@fastmail.com>
2024-02-10 10:04:21 +0100

    Refactor `TodoServer` to use a custom generic server process

--- lib/todo.ex
+++ lib/todo.ex
@@ -21,63 +21,90 @@ defmodule TodoServer do
   ]
 
   """
-  require Logger
 
-  def start() do
-    spawn(fn -> loop(TodoList.new()) end)
+  def init do
+    TodoList.new()
+  end
+
+  def start do
+    ServerProcess.start(TodoServer)
   end
 
   def add_entry(todo_server, new_entry) do
-    send(todo_server, {:add_entry, new_entry})
+    ServerProcess.cast(todo_server, {:add_entry, new_entry})
   end
 
   def entries(todo_server, date) do
-    send(todo_server, {:entries, self(), date})
+    ServerProcess.call(todo_server, {:entries, date})
+  end
+
+  def update_entry(todo_server, %{} = new_entry) do
+    ServerProcess.cast(todo_server, {:update_entry, new_entry})
+  end
+
+  def delete_entry(todo_server, entry_id) do
+    ServerProcess.cast(todo_server, {:delete_entry, entry_id})
+  end
+
+  def handle_cast({:add_entry, new_entry}, todo_list) do
+    TodoList.add_entry(todo_list, new_entry)
+  end
+
+  def handle_cast({:update_entry, new_entry}, todo_list) do
+    TodoList.update_entry(todo_list, new_entry)
+  end
+
+  def handle_cast({:delete_entry, entry_id}, todo_list) do
+    TodoList.delete_entry(todo_list, entry_id)
+  end
+
+  def handle_call({:entries, date}, todo_list) do
+    {TodoList.entries(todo_list, date), todo_list}
+  end
+end
+
+defmodule ServerProcess do
+  @moduledoc """
+  Custom implementation of a generic server process.
+  """
+  require Logger
+
+  def start(callback_module) do
+    spawn(fn ->
+      initial_state = callback_module.init()
+      loop(callback_module, initial_state)
+    end)
+  end
+
+  defp loop(callback_module, current_state) do
+    receive do
+      {:call, request, caller} ->
+        {response, new_state} = callback_module.handle_call(request, current_state)
+        send(caller, {:response, response})
+        loop(callback_module, new_state)
+
+      {:cast, request} ->
+        new_state = callback_module.handle_cast(request, current_state)
+        loop(callback_module, new_state)
+
+      unknown_message ->
+        Logger.error("Received unknown message: #{inspect(unknown_message)}")
+        loop(callback_module, current_state)
+    end
+  end
+
+  def call(server_pid, request) do
+    send(server_pid, {:call, request, self()})
 
     receive do
-      {:todo_entries, entries} -> entries
+      {:response, response} -> response
     after
       5000 -> {:error, :timeout}
     end
   end
 
-  def update_entry(todo_server, %{} = new_entry) do
-    send(todo_server, {:update_entry, new_entry})
-  end
-
-  def delete_entry(todo_server, entry_id) do
-    send(todo_server, {:delete_entry, entry_id})
-  end
-
-  defp loop(todo_list) do
-    new_todo_list =
-      receive do
-        message -> process_message(todo_list, message)
-      end
-
-    loop(new_todo_list)
-  end
-
-  defp process_message(todo_list, {:add_entry, new_entry}) do
-    TodoList.add_entry(todo_list, new_entry)
-  end
-
-  defp process_message(todo_list, {:entries, caller, date}) do
-    send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
-    todo_list
-  end
-
-  defp process_message(todo_list, {:update_entry, new_entry}) do
-    TodoList.update_entry(todo_list, new_entry)
-  end
-
-  defp process_message(todo_list, {:delete_entry, entry_id}) do
-    TodoList.delete_entry(todo_list, entry_id)
-  end
-
-  defp process_message(todo_list, unknown_message) do
-    Logger.error("Received unknown message: #{inspect(unknown_message)}")
-    todo_list
+  def cast(server_pid, request) do
+    send(server_pid, {:cast, request})
   end
 end
 
