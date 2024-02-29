71640c442d205e232269c0a014f23d84a00f45a6
José Fernández Ramos <jframos@fastmail.com>
2024-02-29 12:54:51 +0100

    Reimplement `Todo.System` supervisor using a callback module

--- lib/todo/system.ex
+++ lib/todo/system.ex
@@ -3,7 +3,15 @@ defmodule Todo.System do
   To-do system supervisor.
   """
 
+  use Supervisor
+
   def start_link do
-    Supervisor.start_link([Todo.Cache], strategy: :one_for_one)
+    # Starts the supervisor with `Todo.System` as the callback module
+    Supervisor.start_link(__MODULE__, nil)
+  end
+
+  def init(_) do
+    # Impements the required callback function
+    Supervisor.init([Todo.Cache], strategy: :one_for_one)
   end
 end
