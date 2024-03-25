b471cfd8f7d4f84ba4e7b0e9df7f4121c8cee75f
José Fernández Ramos <jframos@fastmail.com>
2024-03-25 10:26:54 +0100

    Global registration of to-do servers

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -29,7 +29,7 @@ defmodule Todo.Server do
   @expiry_idle_timeout :timer.seconds(30)
 
   def start_link(name) do
-    GenServer.start_link(__MODULE__, name, name: via_tuple(name))
+    GenServer.start_link(__MODULE__, name, name: global_name(name))
   end
 
   def add_entry(todo_server, new_entry) do
@@ -95,7 +95,7 @@ defmodule Todo.Server do
     {:stop, :normal, {name, todo_list}}
   end
 
-  defp via_tuple(name) do
-    Todo.ProcessRegistry.via_tuple({__MODULE__, name})
+  defp global_name(name) do
+    {:global, {__MODULE__, name}}
   end
 end
